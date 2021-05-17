4827f7381f6e5ff367456d8d1a7cb71608ded7e4
Parveen Kumar <sudopk@google.com>
2021-05-16 17:29:29 -0700

    Add support for maven publishing

--- KAndroidCommon/build.gradle.kts
+++ KAndroidCommon/build.gradle.kts
@@ -1,26 +1,47 @@
 plugins {
-    id("com.android.library")
-    id("kotlin-android")
+  id("com.android.library")
+  id("kotlin-android")
+  id("maven-publish")
 }
 
 android {
-    compileSdk = 30
+  compileSdk = 30
 
-    defaultConfig {
-        minSdk = 14
-        targetSdk = 30
-    }
+  defaultConfig {
+    minSdk = 14
+    targetSdk = 30
+  }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
+  buildTypes {
+    getByName("release") {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
     }
+  }
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.3.0")
+  implementation("com.google.android.material:material:1.3.0")
 
-    testImplementation("junit:junit:4.13.2")
+  testImplementation("junit:junit:4.13.2")
+}
+
+// https://developer.android.com/studio/build/maven-publish-plugin
+// Because the components are created only during the afterEvaluate phase, you must
+// configure your publications using the afterEvaluate() lifecycle method.
+afterEvaluate {
+  publishing {
+    publications {
+      // Creates a Maven publication called "release".
+      create<MavenPublication>("release") {
+        // Applies the component for the release build variant.
+        from(components["release"])
+
+        // You can then customize attributes of the publication as shown below.
+        groupId = "com.github.sudopk"
+        artifactId = "KAndroid"
+        version = "1.1.1"
+      }
+    }
+  }
 }
